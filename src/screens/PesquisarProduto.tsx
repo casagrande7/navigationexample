b20b80eb557ced2ed19710574710aa73bec82703
casagrande7 <joaofootz10@gmail.com>
2024-05-02 11:10:09 -0300

    At

--- src/screens/PesquisarProduto.tsx
+++ src/screens/PesquisarProduto.tsx
@@ -14,4 +14,33 @@ function PesquisarProduto(): React.JSX.Element {
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    menuItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+        marginTop: 10
+    },
+    image: {
+        width: 80,
+        height: 80,
+        resizeMode: 'cover',
+        borderRadius: 5
+    },
+    itemDetails: {
+        marginLeft: 10,
+        flex: 1
+    },
+    name: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+
+});
+
 export default PesquisarProduto;
